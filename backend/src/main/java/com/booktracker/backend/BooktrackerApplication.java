4f776bd2ed239519a1743c0a09ec8ec5e8db83b5
2018csb1082 <daksh1sharma1@gmail.com>
2025-07-04 11:25:43 +0530

    Implemented h2 database in Spring

--- backend/src/main/java/com/booktracker/backend/BooktrackerApplication.java
+++ backend/src/main/java/com/booktracker/backend/BooktrackerApplication.java
@@ -1,13 +1,23 @@
 package com.booktracker.backend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BooktrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BooktrackerApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner seedDatabase(BookRepository repository) {
+			return args -> {
+					repository.save(new Book("Atomic Habits", "James Clear", "Self-Help", "https://covers.openlibrary.org/b/id/10594752-L.jpg"));
+					repository.save(new Book("Dune", "Frank Herbert", "Science Fiction", "https://covers.openlibrary.org/b/id/10318328-L.jpg"));
+			};
 	}
 
 }
